08de39cb01efe30c5acce68b7a988904c9a443ef
NavyaSriB <basava@thoughtworks.com>
2017-04-06 18:49:18 +0530

    app in demo state

--- app/src/main/java/com/example/basava/demoapp/WelcomeActivity.java
+++ app/src/main/java/com/example/basava/demoapp/WelcomeActivity.java
@@ -1,19 +1,19 @@
 package com.example.basava.demoapp;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
-public class WelcomeActivity extends Activity {
+public class WelcomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome_activity);
         Intent intent = getIntent();
         String name = (String) intent.getExtras().get("name");
-        TextView textView = (TextView) findViewById(R.id.message);
-        String welcomeText = "Welcome to your new android App \n" + name;
+        TextView textView = (TextView) findViewById(R.id.name);
+        String welcomeText = textView.getText() + name;
         textView.setText(welcomeText);
     }
 }
